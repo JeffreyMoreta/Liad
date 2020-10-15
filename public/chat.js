028ac7a024dd157ef5c2416df899ce596fe2b329
Jeffrey <jeffreymoreta@gmail.com>
2020-10-15 07:50:08 -0400

    Updated Lounge

--- public/chat.js
+++ public/chat.js
@@ -25,14 +25,44 @@ chatForm.addEventListener("submit", (e) => {
   e.target.elements.msg.focus();
 });
 
+function chatBubble(message){
+  if(message.username === username){
+    return `
+    <div class="message-right ms1">
+      <div class="messageinner-ms1">
+        <p class="message-text">
+          <span style="
+            display: inline-block; 
+            position:relative; 
+            right: -10%; 
+            font-size: 10px;
+            color: lightgrey;">${message.username} ${message.time}</span>
+          <span style="display: block;">${message.text}</span>
+        </p>
+      </div>
+    </div>
+    `
+  } else {
+    return `
+    <div class="message-left ms2">
+      <div class="messageinner-ms2">
+        <p class="message-text">
+          <span style="
+            display: inline-block; 
+            position: relative;
+            font-size: 10px;
+            color: grey;">${message.username} ${message.time}</span>
+          <span style="display: block;">${message.text}</span>
+        </p>
+      </div>
+    </div>
+    `
+  }
+}
+
 function outputMessage(message) {
   const div = document.createElement("div");
   div.classList.add("message");
-  div.innerHTML = `
-  <p class="meta">${message.username}
-    <span>${message.time}</span>
-    <span class='text'>${message.text} </span>
-  </p>
-  `;
+  div.innerHTML = chatBubble(message);
   document.querySelector(".chat-messages").appendChild(div);
 }
